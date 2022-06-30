337b5f62624502a172efc0cfdd06549a19738642
Rabee <rabee@Talals-MacBook-Air.local>
2022-06-30 00:59:18 -0400

    add colour picker

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 //set important constants and variables
 const gridContainer = document.getElementById("grid-container")
+const colorPicker = document.getElementById('colorPicker')
+const colorBtn = document.getElementById('colorBtn')
+let penColour = 'black'
 
 function destroyGrid() {
     while(gridContainer.firstChild) {
@@ -18,7 +21,7 @@ function makeGrid(rows, cols) {
     let gridItems = document.getElementsByClassName('grid-item');
 
     let colourCell = function(elem) {
-        elem.target.style.background = 'black';
+        elem.target.style.background = penColour;
     };
 
     for (let i = 0; i < gridItems.length; i++) {
@@ -26,7 +29,7 @@ function makeGrid(rows, cols) {
 }
 };
 
-makeGrid(5, 5); //create grid
+makeGrid(20, 20); //create grid
 
 //add resetButton functionality
 let resetBoard = function() {
@@ -51,3 +54,5 @@ sizeBtn.addEventListener('click', ()=> {
         resetBoard;
     }
 });
+
+colorPicker.oninput = (e) => penColour = e.target.value
